700af2818151541274ebb03c137412a502111897
Akshay3237 <akshaygohel364@gmail.com>
2024-03-07 21:47:09 +0530

    this is new

--- src/main/java/parking/management/enity/User.java
+++ src/main/java/parking/management/enity/User.java
@@ -1,5 +1,23 @@
 package parking.management.enity;
 
-public class User {
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int user_id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone_number;
+
+    
 }
+
